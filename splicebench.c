e9dc9e9f12de470cbd4ec90d8a6dc33f22fc7591
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-06-01 00:46:18 +0200

    Implement copy mode in a forked process.

--- splicebench.c
+++ splicebench.c
@@ -17,6 +17,7 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include <err.h>
 #include <errno.h>
@@ -32,10 +33,18 @@ int splicemode;
 char *listenhost, *bindouthost, *connecthost;
 char *listenport, *bindoutport, *connectport;
 
+struct ev_splice {
+	struct	event ev;
+	int	sock;
+};
+
 void	socket_listen(void);
 void	accepting_cb(int, short, void *);
 void	connected_cb(int, short, void *);
+void	socket_splice(struct ev_splice *, int, int);
 void	unsplice_cb(int, short, void *);
+void	process_copy(struct ev_splice *, int, int);
+void	waitpid_cb(int, short, void *);
 int	socket_connect(const char *, const char *, const char *, const char *,
 	    struct addrinfo *);
 int	socket_bind_connect(struct addrinfo *, const char *,
@@ -158,11 +167,6 @@ socket_listen(void)
 	event_add(ev, NULL);
 }
 
-struct ev_splice {
-	struct	event ev;
-	int	sock;
-};
-
 void
 accepting_cb(int lsock, short event, void *arg)
 {
@@ -237,33 +241,109 @@ connected_cb(int csock, short event, void *arg)
 		errx(1, "getnameinfo: %s", gai_strerror(error));
 	printf("connect peer: %s %s\n", host, serv);
 
+	if (splicemode)
+		socket_splice(evs, asock, csock);
+	else
+		process_copy(evs, asock, csock);
+}
 
-	if (setsockopt(asock, SOL_SOCKET, SO_SPLICE, &csock, sizeof(int)) < 0)
+void
+socket_splice(struct ev_splice *evs, int from, int to)
+{
+	if (setsockopt(from, SOL_SOCKET, SO_SPLICE, &to, sizeof(int)) < 0)
 		err(1, "setsockopt SO_SPLICE");
 
-	event_set(&evs->ev, asock, EV_READ, unsplice_cb, evs);
-	evs->sock = csock;
+	event_set(&evs->ev, from, EV_READ, unsplice_cb, evs);
+	evs->sock = to;
 	event_add(&evs->ev, NULL);
 }
 
 void
-unsplice_cb(int asock, short event, void *arg)
+unsplice_cb(int from, short event, void *arg)
 {
 	struct ev_splice *evs = arg;
-	int csock = evs->sock;
+	int to = evs->sock;
 	socklen_t len;
 	int error;
 
 	len = sizeof(int);
-	if (getsockopt(asock, SOL_SOCKET, SO_ERROR, &error, &len) < 0)
+	if (getsockopt(from, SOL_SOCKET, SO_ERROR, &error, &len) < 0)
 		err(1, "getsockopt SO_ERROR");
 	if (error) {
 		errno = error;
 		err(1, "splice");
 	}
 
-	close(asock);
-	close(csock);
+	close(from);
+	close(to);
+	free(evs);
+}
+
+void
+process_copy(struct ev_splice *evs, int from, int to)
+{
+	int pfds[2];
+	pid_t child;
+
+	if (pipe(pfds) < 0)
+		err(1, "pipe");
+	if (fflush(stdout) != 0)
+		err(1, "fflush");
+
+	/* fork a process so that multiple streams copy in parallel */
+	child = fork();
+	if (child < 0)
+		err(1, "fork");
+
+	if (child == 0) {
+		/* child */
+		char *buf;
+
+		if (close(pfds[0]))
+			err(1, "close");
+		if ((buf = malloc(10*1024*1024)) == NULL)
+			err(1, "malloc copy buf");
+		for (;;) {
+			ssize_t in, out;
+
+			in = read(from, buf, 10*1024*1024);
+			if (in < 0)
+				err(1, "read");
+			if (in == 0)
+				break;
+			out = write(to, buf, in);
+			if (out < 0)
+				err(1, "write");
+			if (out != in)
+				errx(1, "partial write %zd of %zd", out, in);
+		}
+		_exit(0);
+	}
+
+	/* parent */
+	if (close(pfds[1]))
+		err(1, "close");
+	close(from);
+	close(to);
+
+	event_set(&evs->ev, pfds[0], EV_READ, waitpid_cb, evs);
+	evs->sock = child;
+	event_add(&evs->ev, NULL);
+}
+
+void
+waitpid_cb(int pfd, short event, void *arg)
+{
+	struct ev_splice *evs = arg;
+	pid_t child = evs->sock;
+	int status;
+
+	if (waitpid(child, &status, 0))
+		err(1, "waitpid");
+	if (status != 0)
+		errx(1, "copy child: %d", status);
+
+	close(pfd);
 	free(evs);
 }
 
